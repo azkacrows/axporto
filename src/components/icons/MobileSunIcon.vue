eef89517f03eb780dee3826d72d506f453caed9a
azkacrows <azkacrows@gmail.com>
2024-03-15 01:35:58 +0700

    update

--- src/components/icons/MobileSunIcon.vue
+++ src/components/icons/MobileSunIcon.vue
@@ -1,6 +1,6 @@
 <template>
     <svg
-        class="w-6 h-6 fill-current swap-on"
+        class="w-6 h-6 fill-current swap-off"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
     >
