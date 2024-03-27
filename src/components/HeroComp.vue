32a7c89202c0992426ad45b05fca9e93064ef07b
azkacrows <azkacrows@gmail.com>
2024-03-28 00:01:19 +0700

    update

--- src/components/HeroComp.vue
+++ src/components/HeroComp.vue
@@ -2,6 +2,7 @@
 import { RouterLink } from 'vue-router';
 
 import PhotoBGIcon from './icons/PhotoBGIcon.vue';
+import DotFGIcon from './icons/DotFGIcon.vue';
 </script>
 
 <template>
@@ -18,23 +19,33 @@ import PhotoBGIcon from './icons/PhotoBGIcon.vue';
                     Welcome to the Canvas of Innovation! It's great to have you here. If you love
                     all things art, design, and code — you're in the right place.
                 </p>
-                <p class="pt-6 lg:hidden">
+                <p class="py-6 lg:hidden">
                     He crafts responsive websites where technologies meet creativity
                 </p>
-
                 <button class="btn betton">
                     <router-link to="/contact">Let's Talk!!</router-link>
                 </button>
             </div>
             <div class="relative flex flex-col items-center justify-end lg:flex-row lg:w-5/12">
-                <div class="absolute z-10 w-44">
+                <div class="absolute z-10 w-44 top-8 left-14">
                     <PhotoBGIcon />
                 </div>
-                <div class="relative z-20 w-70 left-10 lg:left-0">
+                <div class="relative z-20 w-[15.5rem] left-11">
                     <img src="../assets/img/Mc.png" class="object-scale-down" alt="illustration" />
                 </div>
-                <div></div>
-                <div></div>
+                <div class="absolute z-30 right-2 bottom-24">
+                    <DotFGIcon />
+                </div>
+                <div class="relative z-30">
+                    <div
+                        class="flex flex-row items-center gap-2 p-2 h-auto w-auto border border-solid border-[#ABB2BF] bg-[#282C33]"
+                    >
+                        <div
+                            class="h-4 w-4 bg-[#c778dd] border border-solid border-[#c778dd]"
+                        ></div>
+                        <p>Currently working on <span>Portfolio</span></p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
