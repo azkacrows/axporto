056f1f756f4e24ba31282994afc5d32bde9366d3
azkacrows <azkacrows@gmail.com>
2024-06-03 19:58:46 +0700

    update

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -1,47 +1,48 @@
-<!-- TODO : About Pages -->
 <template>
-    <div class="container mx-auto">
-        <!-- About header -->
-        <div class="mt-12">
-            <div class="flex flex-row items-center font-fira-code">
-                <div class="flex flex-col items-start justify-between w-full gap-4">
-                    <h2 class="pl-4 text-4xl font-semibold lg:pl-0 text-accent">
-                        <span class="text-primary">/</span>about-me
-                    </h2>
-                    <p>Who am i?</p>
-                </div>
+    <!-- Contact header -->
+    <div class="mt-12">
+        <div class="flex flex-row items-center font-fira-code">
+            <div class="flex flex-row items-center justify-between w-full gap-4 pl-4">
+                <h2 class="text-4xl font-semibold lg:pl-0 text-accent">
+                    <span class="text-primary">/</span>about-me
+                </h2>
             </div>
         </div>
-        <!-- About Content -->
-        <div class="flex flex-row items-center justify-between gap-10 my-10 font-fira-code">
-            <!-- left side -->
-            <div class="flex flex-col w-1/2 gap-6">
-                <p>illustration Hello, i’m Jamal!</p>
-                <p>
-                    I’m a self-taught front-end developer based in East java, Indonesia. I can
-                    develop responsive websites from scratch and raise them into modern
-                    user-friendly web experiences.
-                </p>
-                <p>
-                    For over a year, I've been transforming my creativity and expertise into
-                    websites. I've been assisting many clients in establishing their web presence. I
-                    am always looking for new technologies and frameworks to learn about.
-                </p>
-            </div>
-            <!-- right side -->
-            <div class="relative flex flex-col w-1/2">
+    </div>
+    <!-- Contact Content -->
+    <div
+        class="flex flex-col items-center justify-between gap-4 px-4 my-10 lg:gap-0 lg:flex-row font-fira-code"
+    >
+        <!-- left side -->
+        <div class="flex flex-col max-w-lg gap-6 text-center lg:text-start">
+            <!-- <p class="pr-4">
+                I’m interested in freelance opportunities. However, if you have other request or
+                question, don’t hesitate to contact me through social media or drop me a message in
+                form contact!
+            </p> -->
+            <p>Hello, i’m Jamal!</p>
+            <p>
+                I’m a self-taught front-end developer based in East java, Indonesia. I can develop
+                responsive websites from scratch and raise them into modern user-friendly web
+                experiences.
+            </p>
+            <p>
+                For over a year, I've been transforming my creativity and expertise into websites.
+                I've been assisting many clients in establishing their web presence. I am always
+                looking for new technologies and frameworks to learn about.
+            </p>
+        </div>
+        <!-- right side -->
+        <div class="w-full max-w-lg m-2">
+            <div class="card">
                 <div class="self-end w-full max-w-md">
-                    <!-- FIXME : Photo -->
                     <div class="relative">
                         <img
                             src="../assets/img/Me.png"
                             class="object-scale-down contrast-125 saturate-200"
                             alt=""
                         />
-                        <hr
-                            class="absolute flex w-60 h-3 border-0 -z-10 bottom-[30px] right-24 bg-primary rotate-[10deg]"
-                        />
-                        <hr class="absolute flex w-full h-px border-0 bg-primary" />
+                        <hr class="flex w-full h-px border-0 bg-primary" />
                     </div>
                 </div>
             </div>
