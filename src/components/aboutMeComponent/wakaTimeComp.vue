0476af2735f398f3b85cd1a1bf145bf517683ae5
azkacrows <azkacrows@gmail.com>
2024-06-08 12:58:43 +0700

    update

---
+++ src/components/aboutMeComponent/wakaTimeComp.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/no-parsing-error -->
+<template>
+    <div>
+        <figure><embed src="https://wakatime.com/share/@9b664592-9aab-459c-a287-0d6ab348189b/23ffbee1-53c2-45a0-b858-9b56942a8882.svg"></embed></figure>
+    </div>
+</template>
