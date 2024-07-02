eb9ae6aee52b671eac780c791087c273996dcdf8
azkacrows <azkacrows@gmail.com>
2024-07-02 14:09:59 +0700

    update some

--- src/components/certificationComponent/CertificationComp.vue
+++ src/components/certificationComponent/CertificationComp.vue
@@ -1,9 +1,26 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    Url: String,
+    FileId: String,
+});
+
+const source = computed(
+    () => `https://drive.google.com/thumbnail?authuser=0&sz=w1080&id=${props.FileId}`
+);
+</script>
 
 <template>
-    <div
-        class="flex flex-col p-5 mx-4 mt-2 border-2 border-solid lg:flex-row lg:mx-16 border-success/40 rounded-3xl lg:rounded-tl-none lg:rounded-br-none"
-    ></div>
+    <div class="certInner">
+        <div class="w-full carousel-item">
+            <img :src="source" class="object-scale-down w-full rounded-lg lg:rounded-xl" />
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.certInner {
+    @apply flex p-1 lg:p-2 w-full carousel;
+}
+</style>
--- src/views/CertificationView.vue
+++ src/views/CertificationView.vue
@@ -1,5 +1,27 @@
 <script setup>
+import { ref, onMounted, computed } from 'vue';
 import CertificationComp from '../components/certificationComponent/CertificationComp.vue';
+import certificationList from '../stores/certList.json';
+
+const mainCertList = ref([]);
+const currentIndex = ref(0);
+
+onMounted(() => {
+    const mainFolder = certificationList[0].Sertifikat.find((folder) => folder.Folder === 'Main');
+    mainCertList.value = mainFolder ? mainFolder.ListSertifikat : [];
+});
+
+const totalCertificates = computed(() => mainCertList.value.length);
+const currentCert = computed(() => mainCertList.value[currentIndex.value]);
+
+const goToNext = () => {
+    currentIndex.value = (currentIndex.value + 1) % totalCertificates.value;
+};
+
+const goToPrevious = () => {
+    currentIndex.value =
+        (currentIndex.value - 1 + totalCertificates.value) % totalCertificates.value;
+};
 </script>
 
 <template>
@@ -12,7 +34,9 @@ import CertificationComp from '../components/certificationComponent/Certificatio
                 <h2 class="text-4xl font-semibold lg:pl-0 text-accent">
                     <span class="text-primary">/</span>certification
                 </h2>
-                <p>certifications i've acquired thus far</p>
+                <p class="mx-8 text-center lg:mx-0 lg:text-start text-wrap lg:text-nowrap">
+                    certifications i've acquired thus far
+                </p>
             </div>
         </div>
     </div>
@@ -20,8 +44,38 @@ import CertificationComp from '../components/certificationComponent/Certificatio
     <div
         class="flex flex-col items-center justify-between gap-4 px-4 my-10 lg:gap-0 lg:flex-row font-fira-code"
     >
-        <CertificationComp />
+        <div class="certOuter">
+            <CertificationComp
+                v-if="currentCert"
+                :Url="currentCert.Url"
+                :FileId="currentCert.FileId"
+                :index="currentIndex"
+                :total="totalCertificates"
+            />
+            <div class="btnWrapper">
+                <button @click="goToPrevious" class="btnPrevNext !rounded-bl-md lg:!rounded-bl-lg">
+                    ❮
+                </button>
+                <a :href="currentCert?.Url" target="_blank" class="btnPrevNext !px-6 lg:!btn-wide"
+                    >Visit</a
+                >
+                <button @click="goToNext" class="btnPrevNext !rounded-br-md lg:!rounded-br-lg">
+                    ❯
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.certOuter {
+    @apply flex relative flex-col mx-auto border-2 border-solid lg:w-3/4 border-success/40 rounded-lg lg:rounded-xl;
+}
+
+.btnWrapper {
+    @apply flex absolute justify-between w-full bottom-0 bg-base-100 bg-opacity-70;
+}
+.btnPrevNext {
+    @apply btn btn-accent text-accent-content btn-square rounded-none btn-xs lg:btn-md;
+}
+</style>
